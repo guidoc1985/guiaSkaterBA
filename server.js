84d2267330bc92095f8951dd41097d59b94176c7
guido cardama <you@example.com>
2024-10-21 17:15:52 -0300

    intento 6

--- server.js
+++ server.js
@@ -23,20 +23,19 @@ app.use('/libs', express.static(path.join(__dirname, 'libs')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const dbConfig = mysql.createConnection(process.env.JAWSDB_URL);
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database!');
-});
+
+
+  // Conectar a la base de datos
+  dbConfig.connect((err) => {
+      if (err) {
+          console.error('Error al conectar a la base de datos: ' + err.stack);
+          return;
+      }
+      console.log('Conectado a la base de datos como ID ' + connection.threadId);
+  });
+  
 
 
 app.get('/', (req, res) => {
